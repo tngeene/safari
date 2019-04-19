3c4dde6640acdd015dc714298ba8cc85f4098aaa
_klemo <ronoclement86@gmail.com>
2019-04-19 11:21:29 +0300

    modified overview

--- app/models/country.py
+++ app/models/country.py
@@ -8,7 +8,7 @@ class Country(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True)
     image_url = db.Column(db.String(64), index=True)
-    overview = db.Column(db.String(10000), index=True)
+    overview = db.Column(db.String(10000))
     description = db.Column(db.String(10000))
     climate = db.Column(db.String(500), index=True)
     best_time_to_visit = db.Column(db.String(500))
