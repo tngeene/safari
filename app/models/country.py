0fef6a24f264e5f6ae514c2d6d330775e5ba4c6d
_klemo <ronoclement86@gmail.com>
2019-06-12 10:39:21 +0300

    migrations will be changed

--- app/models/country.py
+++ app/models/country.py
@@ -2,31 +2,32 @@ from datetime import datetime
 from .. import db
 
 
-
 class Country(db.Model):
-    __tablename__ = 'countries'
+    __tablename__ = "countries"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True)
     image_url = db.Column(db.String(64), index=True)
-    overview = db.Column(db.String(10000))
     description = db.Column(db.String(10000))
     climate = db.Column(db.String(500), index=True)
     best_time_to_visit = db.Column(db.String(500))
-    parks = db.relationship('Park', backref='country', lazy='dynamic')
+    parks = db.relationship("Park", backref="country", lazy="dynamic")
     timestamp = db.Column(db.DateTime(), default=datetime.utcnow)
     createdAt = db.Column(db.DateTime(), default=datetime.utcnow)
-    updatedAt = db.Column(db.DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)
+    updatedAt = db.Column(
+        db.DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow
+    )
 
     def __repr__(self):
-        return '<Country \'%s\'>' % self.name
+        return "<Country '%s'>" % self.name
 
     @staticmethod
     def get_country_code(country):
         from app.publisher.countries import get_arcode
+
         code = get_arcode()
         if country.lower().title() in code:
             county_code = code[country.lower().title()]
         else:
-            county_code = 'KE'
+            county_code = "KE"
 
         return county_code
--- config.py
+++ config.py
@@ -11,69 +11,78 @@ else:
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-if os.path.exists('.env'):
-    print('Importing environment from .env file')
-    for line in open('.env'):
-        var = line.strip().split('=')
+if os.path.exists(".env"):
+    print("Importing environment from .env file")
+    for line in open(".env"):
+        var = line.strip().split("=")
         if len(var) == 2:
-            os.environ[var[0]] = var[1].replace("\"", "")
+            os.environ[var[0]] = var[1].replace('"', "")
 
 
 class Config:
-    APP_NAME = os.environ.get('APP_NAME') or 'Safari'
+    APP_NAME = os.environ.get("APP_NAME") or "Safari"
 
-    if os.environ.get('SECRET_KEY'):
-        SECRET_KEY = os.environ.get('SECRET_KEY')
+    if os.environ.get("SECRET_KEY"):
+        SECRET_KEY = os.environ.get("SECRET_KEY")
     else:
-        SECRET_KEY = 'SECRET_KEY_ENV_VAR_NOT_SET'
-        print('SECRET KEY ENV VAR NOT SET! SHOULD NOT SEE IN PRODUCTION')
+        SECRET_KEY = "SECRET_KEY_ENV_VAR_NOT_SET"
+        print("SECRET KEY ENV VAR NOT SET! SHOULD NOT SEE IN PRODUCTION")
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 
     # Email
-    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.sendgrid.net'
-    MAIL_PORT = os.environ.get('MAIL_PORT') or 587
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or True
-    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') or False
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
+    MAIL_SERVER = os.environ.get("MAIL_SERVER") or "smtp.sendgrid.net"
+    MAIL_PORT = os.environ.get("MAIL_PORT") or 587
+    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") or True
+    MAIL_USE_SSL = os.environ.get("MAIL_USE_SSL") or False
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
 
     # Analytics
-    GOOGLE_ANALYTICS_ID = os.environ.get('GOOGLE_ANALYTICS_ID') or ''
-    SEGMENT_API_KEY = os.environ.get('SEGMENT_API_KEY') or ''
+    GOOGLE_ANALYTICS_ID = os.environ.get("GOOGLE_ANALYTICS_ID") or ""
+    SEGMENT_API_KEY = os.environ.get("SEGMENT_API_KEY") or ""
 
     # Admin account
-    ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD') or '1con'
-    ADMIN_EMAIL = os.environ.get(
-        'ADMIN_EMAIL') or 'iroshbrian@gmail.com'
-    EMAIL_SUBJECT_PREFIX = '[{}]'.format(APP_NAME)
-    EMAIL_SENDER = '{app_name} Admin <{email}>'.format(
-        app_name=APP_NAME, email=MAIL_USERNAME)
+    ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD") or "1con"
+    ADMIN_EMAIL = os.environ.get("ADMIN_EMAIL") or "iroshbrian@gmail.com"
+    EMAIL_SUBJECT_PREFIX = "[{}]".format(APP_NAME)
+    EMAIL_SENDER = "{app_name} Admin <{email}>".format(
+        app_name=APP_NAME, email=MAIL_USERNAME
+    )
 
     # 1st User account
-    USER_PASSWORD = os.environ.get('USER_PASSWORD') or '1con'
-    USER_EMAIL = os.environ.get(
-        'USER_EMAIL') or 'app.skye@gmail.com'
+    USER_PASSWORD = os.environ.get("USER_PASSWORD") or "1con"
+    USER_EMAIL = os.environ.get("USER_EMAIL") or "app.skye@gmail.com"
 
-    REDIS_URL = os.getenv('REDISTOGO_URL') or 'http://localhost:6379'
+    REDIS_URL = os.getenv("REDISTOGO_URL") or "http://localhost:6379"
 
-    RAYGUN_APIKEY = os.environ.get('RAYGUN_APIKEY')
+    RAYGUN_APIKEY = os.environ.get("RAYGUN_APIKEY")
 
-    UPLOADED_PHOTOS_DEST = os.path.join(basedir, 'app/static/uploads')
+    UPLOADED_PHOTOS_DEST = os.path.join(basedir, "app/static/uploads")
 
-    UPLOAD_FOLDER=os.path.join(basedir, 'app/static/uploads')
-    UPLOADS_CKEDITOR=os.path.join(basedir, 'app/static/ckeditor_uploads')
-    THUMBNAIL_FOLDER=os.path.join(basedir, 'app/static/uploads/thumbnail')
+    UPLOAD_FOLDER = os.path.join(basedir, "app/static/uploads")
+    UPLOADS_CKEDITOR = os.path.join(basedir, "app/static/ckeditor_uploads")
+    THUMBNAIL_FOLDER = os.path.join(basedir, "app/static/uploads/thumbnail")
 
-    RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_PUBLIC_KEY') or '6LeYIbsSAAAAACRPIllxA7wvXjIE411PfdB2gt2J'
-    RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_PRIVATE_KEY') or '6LeYIbsSAAAAAJezaIq3Ft_hSTo0YtyeFG-JgRtu'
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL"
+    ) or "sqlite:///" + os.path.join(basedir, "data.sqlite")
+
+    RECAPTCHA_PUBLIC_KEY = (
+        os.environ.get("RECAPTCHA_PUBLIC_KEY")
+        or "6LeYIbsSAAAAACRPIllxA7wvXjIE411PfdB2gt2J"
+    )
+    RECAPTCHA_PRIVATE_KEY = (
+        os.environ.get("RECAPTCHA_PRIVATE_KEY")
+        or "6LeYIbsSAAAAAJezaIq3Ft_hSTo0YtyeFG-JgRtu"
+    )
 
     # Parse the REDIS_URL to set RQ config variables
     if PYTHON_VERSION == 3:
-        urllib.parse.uses_netloc.append('redis')
+        urllib.parse.uses_netloc.append("redis")
         url = urllib.parse.urlparse(REDIS_URL)
     else:
-        urlparse.uses_netloc.append('redis')
+        urlparse.uses_netloc.append("redis")
         url = urlparse.urlparse(REDIS_URL)
 
     RQ_DEFAULT_HOST = url.hostname
@@ -93,41 +102,48 @@ class DevelopmentConfig(Config):
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
     #     'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
-
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:safari2019!@localhost/safari2'
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL"
+    ) or "sqlite:///" + os.path.join(basedir, "data.sqlite")
     # DB_URL = 'postgresql+psycopg2://postgres:postgres@localhost/skye'
     #
     # SQLALCHEMY_DATABASE_URI = DB_URL
 
     @classmethod
     def init_app(cls, app):
-        print('THIS APP IS IN DEBUG MODE. \
-                YOU SHOULD NOT SEE THIS IN PRODUCTION.')
+        print(
+            "THIS APP IS IN DEBUG MODE. \
+                YOU SHOULD NOT SEE THIS IN PRODUCTION."
+        )
 
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "TEST_DATABASE_URL"
+    ) or "sqlite:///" + os.path.join(basedir, "data-test.sqlite")
     WTF_CSRF_ENABLED = False
 
     @classmethod
     def init_app(cls, app):
-        print('THIS APP IS IN TESTING MODE.  \
-                YOU SHOULD NOT SEE THIS IN PRODUCTION.')
+        print(
+            "THIS APP IS IN TESTING MODE.  \
+                YOU SHOULD NOT SEE THIS IN PRODUCTION."
+        )
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-    SSL_DISABLE = (os.environ.get('SSL_DISABLE') or 'True') == 'True'
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL"
+    ) or "sqlite:///" + os.path.join(basedir, "data.sqlite")
+    SSL_DISABLE = (os.environ.get("SSL_DISABLE") or "True") == "True"
 
     @classmethod
     def init_app(cls, app):
         Config.init_app(app)
-        assert os.environ.get('SECRET_KEY'), 'SECRET_KEY IS NOT SET!'
+        assert os.environ.get("SECRET_KEY"), "SECRET_KEY IS NOT SET!"
 
-        flask_raygun.Provider(app, app.config['RAYGUN_APIKEY']).attach()
+        flask_raygun.Provider(app, app.config["RAYGUN_APIKEY"]).attach()
 
 
 class HerokuConfig(ProductionConfig):
@@ -137,6 +153,7 @@ class HerokuConfig(ProductionConfig):
 
         # Handle proxy server headers
         from werkzeug.contrib.fixers import ProxyFix
+
         app.wsgi_app = ProxyFix(app.wsgi_app)
 
 
@@ -148,16 +165,17 @@ class UnixConfig(ProductionConfig):
         # Log to syslog
         import logging
         from logging.handlers import SysLogHandler
+
         syslog_handler = SysLogHandler()
         syslog_handler.setLevel(logging.WARNING)
         app.logger.addHandler(syslog_handler)
 
 
 config = {
-    'development': DevelopmentConfig,
-    'testing': TestingConfig,
-    'production': ProductionConfig,
-    'default': DevelopmentConfig,
-    'heroku': HerokuConfig,
-    'unix': UnixConfig
+    "development": DevelopmentConfig,
+    "testing": TestingConfig,
+    "production": ProductionConfig,
+    "default": DevelopmentConfig,
+    "heroku": HerokuConfig,
+    "unix": UnixConfig,
 }
